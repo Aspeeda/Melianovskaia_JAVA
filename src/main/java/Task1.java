46960d7f32942c262c8414ae6a98cef134284b5a
DESKTOP-T1P4KAC\User <aspeeda@yandex.ru>
2023-02-13 16:30:17 +0500

    Init commit

---
+++ src/main/java/Task1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        String nameInBase = "Вячеслав";
+
+        System.out.println("Введите имя: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        if (name.equals(nameInBase)) {
+                System.out.println("Привет, "+ name);
+            }
+        else if  (name.equals(nameInBase.toLowerCase())) {
+                System.out.println("Привет, "+ name);
+        }
+        else {
+            System.out.println("Нет такого имени");
+        }
+        sc.close();
+        }
+    }
+
